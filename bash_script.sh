57e7811bfa92528807492c39ec35056e3dcdfec2
joao-calvo <112580072+joao-calvo@users.noreply.github.com>
2023-10-05 21:38:54 -0300

    Upload Ex2

---
+++ bash_script.sh
@@ -0,0 +1,3 @@
+pwd #Comando para mostrar o caminho para o diretório atual
+ls ~/ > lista_HOME.txt #Comando ls ~/ é usado para listar o conteúdo do diretório HOME, sendo a saída direcianada para o arquivo lista_Home.txt
+date  #Comando usado para imprimir a data atual
